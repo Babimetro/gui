8fbd06a1ca55225fbf38a22bc85b0f4806b6300a
babimtero <babak.izadpanah@gmail.com>
2024-02-02 21:01:51 +0330

    start point

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 18:58:01 2024
+
+@author: babimetro
+"""
+
+import GUI_lib as gl
+
+floder=gl.folderbrowser()
+print("nnnnnnnnnnn",floder)
+
+
+k=gl.filebrowser()
+print("nnnnnnnnnnn",k)
+
+
